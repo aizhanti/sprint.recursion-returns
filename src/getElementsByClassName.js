488e82a1ff402a17ceebc8020fb353d5b5cda43c
Yurika Namba <yurikanamba@gmail.com>
2020-04-08 16:17:30 +0900

    completed getElementsByClassName function
    Co-authored-by: aizhan <aizhan@users.noreply.github.com>

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,20 @@
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (className) => {
+  const nodes = [];
+
+  function findNodes(node) {
+    //base
+    //if element has classes and the array of classes includes the className
+    if (node.classList && node.classList.value.indexOf(className) > -1) {
+      nodes.push(node);
+    }
+
+    //recursive function
+    node.childNodes.forEach((child) => {
+      findNodes(child);
+    });
+  }
+  findNodes(document.body);
+  return nodes;
 };
 
 module.exports = { getElementsByClassName };
